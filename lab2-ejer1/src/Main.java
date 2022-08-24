41fae081ac22c79148fada4d37a6cfe3c658396e
Karen <95293061+KarenJK910@users.noreply.github.com>
2022-08-24 20:12:16 -0300

    ejercicio1

---
+++ lab2-ejer1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	String nombre = "Karen";
+    int edad = 19;
+    float salario = 100.00f;
+    boolean carnet = true;
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Salario: $" + salario);
+        System.out.println("Carnet: " + carnet);
+    }
+}
