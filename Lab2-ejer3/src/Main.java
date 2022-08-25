db8993c895b5a45c8480f61e749c3edf00bb1719
Karen <95293061+KarenJK910@users.noreply.github.com>
2022-08-25 00:11:21 -0300

    tercero

---
+++ Lab2-ejer3/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double sen = Math.sin(0.5);
+        double cos = Math.cos(0.5);
+        double tan = Math.tan(0.5);
+        double exp = Math.exp(2);
+        double lon = Math.log(2.7172);
+        System.out.println(sen);
+        System.out.println(cos);
+        System.out.println(tan);
+        System.out.println(exp);
+        System.out.println(lon);
+        System.out.println(Math.PI);
+
+    }
+}
+
+
