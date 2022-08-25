96de2d1546132757b88eec4c7117c113f1847ebe
Karen <95293061+KarenJK910@users.noreply.github.com>
2022-08-24 23:37:57 -0300

    ejer2

---
+++ lab2-ejer2/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        float num1 = 2;
+        float num2 = 3;
+        float num4 = 7;
+        float num5 = 2;
+        float div1 = (num1 / num2) * (num4 / num5);
+        float num6 = 6;
+        float num7 = 5;
+        float num8 = 2;
+        float num9 = 30;
+        float div2 = num6 / num7 - div1 + num8 / num9;
+        System.out.println(div1);
+        System.out.println(div2);
+        float num10 = 1;
+        float num11 = 3;
+        float num12 = 5;
+        float div3 = (num10 / num11) / num12;
+        System.out.println(div2 / div3);
+    }
+}
